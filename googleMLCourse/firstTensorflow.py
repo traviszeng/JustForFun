00bb29a545306818edb80ec70fabb2552939c2f9
traviszeng <traviszeng7@gmail.com>
2018-03-05 21:46:42 +0800

    add google ML course

---
+++ googleMLCourse/firstTensorflow.py
@@ -0,0 +1,162 @@
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+#导入数据
+california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/ml_universities/california_housing_train.csv", sep=",")
+
+
+#我们将对数据进行随机化处理，
+#以确保不会出现任何病态排序结果（可能会损害随机梯度下降法的效果）。
+#此外，我们会将 median_house_value 调整为以千为单位，
+#这样，模型就能够以常用范围内的学习速率较为轻松地学习这些数据。
+california_housing_dataframe = california_housing_dataframe.reindex(
+    np.random.permutation(california_housing_dataframe.index))
+california_housing_dataframe["median_house_value"] /= 1000.0
+
+print(california_housing_dataframe)
+
+california_housing_dataframe.describe()
+
+
+# Define the input feature: total_rooms.
+my_feature = california_housing_dataframe[["total_rooms"]]
+
+# Configure a numeric feature column for total_rooms.
+feature_columns = [tf.feature_column.numeric_column("total_rooms")]
+
+# Define the label.
+targets = california_housing_dataframe["median_house_value"]
+
+#配置LinearRegressor
+# Use gradient descent as the optimizer for training the model.
+my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+# Configure the linear regression model with our feature columns and optimizer.
+# Set a learning rate of 0.0000001 for Gradient Descent.
+linear_regressor = tf.estimator.LinearRegressor(
+    feature_columns=feature_columns,
+    optimizer=my_optimizer
+)
+
+
+#定义输入函数，告诉TF如何对数据进行预处理，
+#以及模型训练期间如何进行批处理、随机处理和重复数据
+
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    """Trains a linear regression model of one feature.
+  
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+  
+    # Convert pandas data into a dict of np arrays.
+    features = {key:np.array(value) for key,value in dict(features).items()}                                           
+ 
+    # Construct a dataset, and configure batching/repeating
+    ds = Dataset.from_tensor_slices((features,targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    
+    # Shuffle the data, if specified
+    if shuffle:
+      ds = ds.shuffle(buffer_size=10000)
+    
+    # Return the next batch of data
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+#训练模型
+_ = linear_regressor.train(
+    input_fn = lambda:my_input_fn(my_feature, targets),
+    steps=100
+)
+
+#评估模型
+# Create an input function for predictions.
+# Note: Since we're making just one prediction for each example, we don't 
+# need to repeat or shuffle the data here.
+prediction_input_fn =lambda: my_input_fn(my_feature, targets, num_epochs=1, shuffle=False)
+
+# Call predict() on the linear_regressor to make predictions.
+predictions = linear_regressor.predict(input_fn=prediction_input_fn)
+
+# Format predictions as a NumPy array, so we can calculate error metrics.
+predictions = np.array([item['predictions'][0] for item in predictions])
+
+# Print Mean Squared Error and Root Mean Squared Error.
+mean_squared_error = metrics.mean_squared_error(predictions, targets)
+root_mean_squared_error = math.sqrt(mean_squared_error)
+print("Mean Squared Error (on training data): %0.3f" % mean_squared_error)
+print("Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error)
+
+
+#比较一下 RMSE 与目标最大值和最小值的差值
+min_house_value = california_housing_dataframe["median_house_value"].min()
+max_house_value = california_housing_dataframe["median_house_value"].max()
+min_max_difference = max_house_value - min_house_value
+
+print("Min. Median House Value: %0.3f" % min_house_value)
+print("Max. Median House Value: %0.3f" % max_house_value)
+print("Difference between Min. and Max.: %0.3f" % min_max_difference)
+print("Root Mean Squared Error: %0.3f" % root_mean_squared_error)
+
+
+#总体比较一下targets和predictions的情况
+calibration_data = pd.DataFrame()
+calibration_data["predictions"] = pd.Series(predictions)
+calibration_data["targets"] = pd.Series(targets)
+calibration_data.describe()
+
+
+#将获得均匀分布的随机数据样本，以便绘制可辨的散点图
+sample = california_housing_dataframe.sample(n=300)
+
+
+#根据模型的偏差项和特征权重绘制学到的线，并绘制散点图。该线会以红色显示
+# Get the min and max total_rooms values.
+x_0 = sample["total_rooms"].min()
+x_1 = sample["total_rooms"].max()
+
+# Retrieve the final weight and bias generated during training.
+weight = linear_regressor.get_variable_value('linear/linear_model/total_rooms/weights')[0]
+bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
+
+# Get the predicted median_house_values for the min and max total_rooms values.
+y_0 = weight * x_0 + bias 
+y_1 = weight * x_1 + bias
+
+# Plot our regression line from (x_0, y_0) to (x_1, y_1).
+plt.plot([x_0, x_1], [y_0, y_1], c='r')
+
+# Label the graph axes.
+plt.ylabel("median_house_value")
+plt.xlabel("total_rooms")
+
+# Plot a scatter plot from our data sample.
+plt.scatter(sample["total_rooms"], sample["median_house_value"])
+
+# Display graph.
+plt.show()
+
+
+#将上述东西扔进一个函数
+
