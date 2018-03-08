1d83b60dee4948d67055d1863720c1651dbb561a
traviszeng <traviszeng7@gmail.com>
2018-03-08 12:42:05 +0800

    add  feature_sets

---
+++ googleMLCourse/feature_sets.py
@@ -0,0 +1,23 @@
+#goal:
+#创建一个包含极少特征但效果与更复杂的特征集一样出色的集合
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+
+california_housing_dataframe = california_housing_dataframe.reindex(
+    np.random.permutation(california_housing_dataframe.index))
