7683226cd627b0232324a426c21b56c1ec8853a2
traviszeng <traviszeng7@gmail.com>
2018-03-08 13:30:47 +0800

    finish feature_sets

--- googleMLCourse/feature_sets.py
+++ googleMLCourse/feature_sets.py
@@ -266,3 +266,28 @@ _ = train_model(
 
 plt.scatter(training_examples["latitude"], training_targets["median_house_value"])
 #plt.show()
+
+
+#分箱合成特征
+LATITUDE_RANGES = zip(range(32,44),range(33,45))
+
+def select_and_transform_features(source_df):
+  selected_examples = pd.DataFrame()
+  selected_examples["median_income"] = source_df["median_income"]
+  for r in LATITUDE_RANGES:
+    selected_examples["latitude_%d_to_%d" % r] = source_df["latitude"].apply(
+      lambda l: 1.0 if l >= r[0] and l < r[1] else 0.0)
+  return selected_examples
+
+selected_training_examples = select_and_transform_features(training_examples)
+selected_validation_examples = select_and_transform_features(validation_examples)
+
+
+_ = train_model(
+    learning_rate=0.01,
+    steps=500,
+    batch_size=5,
+    training_examples=selected_training_examples,
+    training_targets=training_targets,
+    validation_examples=selected_validation_examples,
+    validation_targets=validation_targets)
