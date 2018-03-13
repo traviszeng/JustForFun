d0352723a70aafef99848f6c58f8ec08640983c7
traviszeng <traviszeng7@gmail.com>
2018-03-13 12:47:55 +0800

    feature_crosses first commit

---
+++ googleMLCourse/feature_crosses.py
@@ -0,0 +1,259 @@
+'''
+通过添加其他合成特征来改进线性回归模型（这是前一个练习的延续）
+使用输入函数将 Pandas DataFrame 对象转换为 Tensors，并在 fit() 和 predict() 中调用输入函数
+使用 FTRL 优化算法进行模型训练
+通过独热编码、分箱和特征组合创建新的合成特征
+'''
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+
+#对数据集进行预处理
+def preprocess_features(california_housing_dataframe):
+    """Prepares input features from California housing data set.
+
+  Args:
+    california_housing_dataframe: A Pandas DataFrame expected to contain data
+      from the California housing data set.
+  Returns:
+    A DataFrame that contains the features to be used for the model, including
+    synthetic features.
+  """
+
+    selected_features = california_housing_dataframe[
+    ["latitude",
+     "longitude",
+     "housing_median_age",
+     "total_rooms",
+     "total_bedrooms",
+     "population",
+     "households",
+     "median_income"]]
+
+    processed_features = selected_features.copy()
+
+    #创建复合特征
+    processed_features["rooms_per_person"] = (california_housing_dataframe["total_rooms"] /california_housing_dataframe["population"])
+
+    return processed_features
+
+#将median_house_value/1000
+def preprocess_targets(california_housing_dataframe):
+  """Prepares target features (i.e., labels) from California housing data set.
+
+  Args:
+    california_housing_dataframe: A Pandas DataFrame expected to contain data
+      from the California housing data set.
+  Returns:
+    A DataFrame that contains the target feature.
+  """
+  output_targets = pd.DataFrame()
+  # Scale the target to be in units of thousands of dollars.
+  output_targets["median_house_value"] = (california_housing_dataframe["median_house_value"] / 1000.0)
+  return output_targets
+
+
+#输入函数
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    """Trains a linear regression model of one feature.
+  
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+    
+    # Convert pandas data into a dict of np arrays.
+    features = {key:np.array(value) for key,value in dict(features).items()}                                           
+ 
+    # Construct a dataset, and configure batching/repeating
+    ds = Dataset.from_tensor_slices((features,targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    
+    # Shuffle the data, if specified
+    if shuffle:
+      ds = ds.shuffle(10000)
+    
+    # Return the next batch of data
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+def construct_feature_columns(input_features):
+  """Construct the TensorFlow Feature Columns.
+
+  Args:
+    input_features: The names of the numerical input features to use.
+  Returns:
+    A set of feature columns
+  """
+  #return set([tf.feature_column.numeric_column(my_feature)
+              #for my_feature in input_features])
+
+"""
+高维度线性模型可受益于使用一种基于梯度的优化方法，叫做 FTRL。
+该算法的优势是针对不同系数以不同方式调整学习速率，如果某些特征很少采用非零值，该算法可能比较实用（也非常适合支持 L1 正则化）。
+我们可以使用 FtrlOptimizer 来应用 FTRL。
+"""
+def train_model(
+    learning_rate,
+    steps,
+    batch_size,
+    feature_columns,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
+  """Trains a linear regression model.
+  
+  In addition to training, this function also prints training progress information,
+  as well as a plot of the training and validation loss over time.
+  
+  Args:
+    learning_rate: A `float`, the learning rate.
+    steps: A non-zero `int`, the total number of training steps. A training step
+      consists of a forward and backward pass using a single batch.
+    feature_columns: A `set` specifying the input feature columns to use.
+    training_examples: A `DataFrame` containing one or more columns from
+      `california_housing_dataframe` to use as input features for training.
+    training_targets: A `DataFrame` containing exactly one column from
+      `california_housing_dataframe` to use as target for training.
+    validation_examples: A `DataFrame` containing one or more columns from
+      `california_housing_dataframe` to use as input features for validation.
+    validation_targets: A `DataFrame` containing exactly one column from
+      `california_housing_dataframe` to use as target for validation.
+      
+  Returns:
+    A `LinearRegressor` object trained on the training data.
+  """
+
+  periods = 10
+  steps_per_period = steps / periods
+
+  # Create a linear regressor object.
+  my_optimizer = tf.train.FtrlOptimizer(learning_rate=learning_rate)
+  my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  linear_regressor = tf.estimator.LinearRegressor(
+      feature_columns=feature_columns,
+      optimizer=my_optimizer
+  )
+  
+  training_input_fn = lambda: my_input_fn(training_examples, 
+                                          training_targets["median_house_value"], 
+                                          batch_size=batch_size)
+  predict_training_input_fn = lambda: my_input_fn(training_examples, 
+                                                  training_targets["median_house_value"], 
+                                                  num_epochs=1, 
+                                                  shuffle=False)
+  predict_validation_input_fn = lambda: my_input_fn(validation_examples, 
+                                                    validation_targets["median_house_value"], 
+                                                    num_epochs=1, 
+                                                    shuffle=False)
+
+  # Train the model, but do so inside a loop so that we can periodically assess
+  # loss metrics.
+  print("Training model...")
+  print("RMSE (on training data):")
+  training_rmse = []
+  validation_rmse = []
+  for period in range (0, periods):
+    # Train the model, starting from the prior state.
+    linear_regressor.train(
+        input_fn=training_input_fn,
+        steps=steps_per_period
+    )
+    # Take a break and compute predictions.
+    training_predictions = linear_regressor.predict(input_fn=predict_training_input_fn)
+    training_predictions = np.array([item['predictions'][0] for item in training_predictions])
+    validation_predictions = linear_regressor.predict(input_fn=predict_validation_input_fn)
+    validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
+    
+    # Compute training and validation loss.
+    training_root_mean_squared_error = math.sqrt(
+        metrics.mean_squared_error(training_predictions, training_targets))
+    validation_root_mean_squared_error = math.sqrt(
+        metrics.mean_squared_error(validation_predictions, validation_targets))
+    # Occasionally print the current loss.
+    print("  period %02d : %0.2f" % (period, training_root_mean_squared_error))
+    # Add the loss metrics from this period to our list.
+    training_rmse.append(training_root_mean_squared_error)
+    validation_rmse.append(validation_root_mean_squared_error)
+  print("Model training finished.")
+
+  
+  # Output a graph of loss metrics over periods.
+  plt.ylabel("RMSE")
+  plt.xlabel("Periods")
+  plt.title("Root Mean Squared Error vs. Periods")
+  plt.tight_layout()
+  plt.plot(training_rmse, label="training")
+  plt.plot(validation_rmse, label="validation")
+  plt.legend()
+
+  return linear_regressor
+
+
+def get_quantile_based_boundaries(feature_values, num_buckets):
+  boundaries = np.arange(1.0, num_buckets) / num_buckets
+  quantiles = feature_values.quantile(boundaries)
+  return [quantiles[q] for q in quantiles.keys()]
+
+# Divide households into 7 buckets.
+households = tf.feature_column.numeric_column("households")
+bucketized_households = tf.feature_column.bucketized_column(
+  households, boundaries=get_quantile_based_boundaries(
+    california_housing_dataframe["households"], 7))
+
+# Divide longitude into 10 buckets.
+longitude = tf.feature_column.numeric_column("longitude")
+bucketized_longitude = tf.feature_column.bucketized_column(
+  longitude, boundaries=get_quantile_based_boundaries(
+    california_housing_dataframe["longitude"], 10))
+
+if __name__=='__main__':
+
+    tf.logging.set_verbosity(tf.logging.ERROR)
+    pd.options.display.max_rows = 10
+    pd.options.display.float_format = '{:.1f}'.format
+
+    california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+
+    #将数据随机化非常重要，不然可能会导致训练集和验证集的分布情况不一
+    california_housing_dataframe = california_housing_dataframe.reindex(
+        np.random.permutation(california_housing_dataframe.index))
+    
+    #选择训练集和验证集
+    training_examples = preprocess_features(california_housing_dataframe.head(12000))
+    training_examples.describe()
+
+    training_targets = preprocess_targets(california_housing_dataframe.head(12000))
+    training_targets.describe()
+
+    validation_examples = preprocess_features(california_housing_dataframe.tail(5000))
+    validation_examples.describe()
+
+    validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
+    validation_targets.describe()
+
+    _ = train_model(
+    learning_rate=1.0,
+    steps=500,
+    batch_size=100,
+    feature_columns=construct_feature_columns(training_examples),
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
