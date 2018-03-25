67bb58be90836fb15a77ffd7713b6c7a06e9d7d3
traviszeng <traviszeng7@gmail.com>
2018-03-25 17:18:48 +0800

    add NN classifier and finish

--- googleMLCourse/class_classification_of_handwritten_digits.py
+++ googleMLCourse/class_classification_of_handwritten_digits.py
@@ -232,6 +232,142 @@ def train_linear_classification_model(
 
   return classifier
 
+
+#用神经网络方法
+def train_nn_classification_model(
+    learning_rate,
+    steps,
+    batch_size,
+    hidden_units,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
+  """Trains a neural network classification model for the MNIST digits dataset.
+  
+  In addition to training, this function also prints training progress information,
+  a plot of the training and validation loss over time, as well as a confusion
+  matrix.
+  
+  Args:
+    learning_rate: An `int`, the learning rate to use.
+    steps: A non-zero `int`, the total number of training steps. A training step
+      consists of a forward and backward pass using a single batch.
+    batch_size: A non-zero `int`, the batch size.
+    hidden_units: A `list` of int values, specifying the number of neurons in each layer.
+    training_examples: A `DataFrame` containing the training features.
+    training_targets: A `DataFrame` containing the training labels.
+    validation_examples: A `DataFrame` containing the validation features.
+    validation_targets: A `DataFrame` containing the validation labels.
+      
+  Returns:
+    The trained `DNNClassifier` object.
+  """
+
+  periods = 10
+  # Caution: input pipelines are reset with each call to train. 
+  # If the number of steps is small, your model may never see most of the data.  
+  # So with multiple `.train` calls like this you may want to control the length 
+  # of training with num_epochs passed to the input_fn. Or, you can do a really-big shuffle, 
+  # or since it's in-memory data, shuffle all the data in the `input_fn`.
+  steps_per_period = steps / periods  
+  # Create the input functions.
+  predict_training_input_fn = create_predict_input_fn(
+    training_examples, training_targets, batch_size)
+  predict_validation_input_fn = create_predict_input_fn(
+    validation_examples, validation_targets, batch_size)
+  training_input_fn = create_training_input_fn(
+    training_examples, training_targets, batch_size)
+  
+  # Create the input functions.
+  predict_training_input_fn = create_predict_input_fn(
+    training_examples, training_targets, batch_size)
+  predict_validation_input_fn = create_predict_input_fn(
+    validation_examples, validation_targets, batch_size)
+  training_input_fn = create_training_input_fn(
+    training_examples, training_targets, batch_size)
+  
+  # Create feature columns.
+  feature_columns = [tf.feature_column.numeric_column('pixels', shape=784)]
+
+  # Create a DNNClassifier object.
+  my_optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
+  my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  classifier = tf.estimator.DNNClassifier(
+      feature_columns=feature_columns,
+      n_classes=10,
+      hidden_units=hidden_units,
+      optimizer=my_optimizer,
+      config=tf.contrib.learn.RunConfig(keep_checkpoint_max=1)
+  )
+
+  # Train the model, but do so inside a loop so that we can periodically assess
+  # loss metrics.
+  print("Training model...")
+  print("LogLoss error (on validation data):")
+  training_errors = []
+  validation_errors = []
+  for period in range (0, periods):
+    # Train the model, starting from the prior state.
+    classifier.train(
+        input_fn=training_input_fn,
+        steps=steps_per_period
+    )
+  
+    # Take a break and compute probabilities.
+    training_predictions = list(classifier.predict(input_fn=predict_training_input_fn))
+    training_probabilities = np.array([item['probabilities'] for item in training_predictions])
+    training_pred_class_id = np.array([item['class_ids'][0] for item in training_predictions])
+    training_pred_one_hot = tf.keras.utils.to_categorical(training_pred_class_id,10)
+        
+    validation_predictions = list(classifier.predict(input_fn=predict_validation_input_fn))
+    validation_probabilities = np.array([item['probabilities'] for item in validation_predictions])    
+    validation_pred_class_id = np.array([item['class_ids'][0] for item in validation_predictions])
+    validation_pred_one_hot = tf.keras.utils.to_categorical(validation_pred_class_id,10)    
+    
+    # Compute training and validation errors.
+    training_log_loss = metrics.log_loss(training_targets, training_pred_one_hot)
+    validation_log_loss = metrics.log_loss(validation_targets, validation_pred_one_hot)
+    # Occasionally print the current loss.
+    print("  period %02d : %0.2f" % (period, validation_log_loss))
+    # Add the loss metrics from this period to our list.
+    training_errors.append(training_log_loss)
+    validation_errors.append(validation_log_loss)
+  print("Model training finished.")
+  # Remove event files to save disk space.
+  _ = map(os.remove, glob.glob(os.path.join(classifier.model_dir, 'events.out.tfevents*')))
+  
+  # Calculate final predictions (not probabilities, as above).
+  final_predictions = classifier.predict(input_fn=predict_validation_input_fn)
+  final_predictions = np.array([item['class_ids'][0] for item in final_predictions])
+  
+  
+  accuracy = metrics.accuracy_score(validation_targets, final_predictions)
+  print("Final accuracy (on validation data): %0.2f" % accuracy)
+
+  # Output a graph of loss metrics over periods.
+  plt.ylabel("LogLoss")
+  plt.xlabel("Periods")
+  plt.title("LogLoss vs. Periods")
+  plt.plot(training_errors, label="training")
+  plt.plot(validation_errors, label="validation")
+  plt.legend()
+  plt.show()
+  
+  # Output a plot of the confusion matrix.
+  cm = metrics.confusion_matrix(validation_targets, final_predictions)
+  # Normalize the confusion matrix by row (i.e by the number of samples
+  # in each class)
+  cm_normalized = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+  ax = sns.heatmap(cm_normalized, cmap="bone_r")
+  ax.set_aspect(1)
+  plt.title("Confusion matrix")
+  plt.ylabel("True label")
+  plt.xlabel("Predicted label")
+  plt.show()
+
+  return classifier
+
 if __name__=='__main__':
     tf.logging.set_verbosity(tf.logging.ERROR)
     pd.options.display.max_rows = 10
@@ -264,7 +400,7 @@ if __name__=='__main__':
     ax.set_title("Label: %i" % training_targets.loc[rand_example])
     ax.grid(False)
 
-    #用linearclassifier进行对比实验
+    #用linearclassifier进行对比实验（准确率90%）
     classifier = train_linear_classification_model(
              learning_rate=0.03,
              steps=1000,
@@ -273,3 +409,70 @@ if __name__=='__main__':
              training_targets=training_targets,
              validation_examples=validation_examples,
              validation_targets=validation_targets)
+
+
+    #ANN的方法
+    classifier = train_nn_classification_model(
+        learning_rate=0.05,
+        steps=1000,
+        batch_size=30,
+        hidden_units=[100, 100],
+        training_examples=training_examples,
+        training_targets=training_targets,
+        validation_examples=validation_examples,
+        validation_targets=validation_targets)
+
+
+    #验证测试集的准确率
+    mnist_test_dataframe = pd.read_csv(
+      "https://storage.googleapis.com/mledu-datasets/mnist_test.csv",
+      sep=",",
+      header=None)
+
+    test_targets, test_examples = parse_labels_and_features(mnist_test_dataframe)
+    test_examples.describe()
+
+    predict_test_input_fn = create_predict_input_fn(
+    test_examples, test_targets, batch_size=100)
+
+    test_predictions = classifier.predict(input_fn=predict_test_input_fn)
+    test_predictions = np.array([item['class_ids'][0] for item in test_predictions])
+  
+    accuracy = metrics.accuracy_score(test_targets, test_predictions)
+    print("Accuracy on test data: %0.2f" % accuracy)
+
+    #可视化第一个隐藏层的权重
+    """
+    我们来花几分钟时间看看模型的 weights_ 属性，以深入探索我们的神经网络，并了解它学到了哪些规律。
+
+    模型的输入层有 784 个权重，对应于 28×28 像素输入图片。
+    第一个隐藏层将有 784×N 个权重，其中 N 指的是该层中的节点数。
+    我们可以将这些权重重新变回 28×28 像素的图片，具体方法是将 N 个 1×784 权重数组变形为 N 个 28×28 大小数组。
+    
+    """
+
+    print(classifier.get_variable_names())
+
+    weights0 = classifier.get_variable_value("dnn/hiddenlayer_0/kernel")
+
+    print("weights0 shape:", weights0.shape)
+
+    num_nodes = weights0.shape[1]
+    num_rows = int(math.ceil(num_nodes / 10.0))
+    fig, axes = plt.subplots(num_rows, 10, figsize=(20, 2 * num_rows))
+    for coef, ax in zip(weights0.T, axes.ravel()):
+        # Weights in coef is reshaped from 1x784 to 28x28.
+        ax.matshow(coef.reshape(28, 28), cmap=plt.cm.pink)
+        ax.set_xticks(())
+        ax.set_yticks(())
+
+    plt.show()
+
+"""
+神经网络的第一个隐藏层应该会对一些级别特别低的特征进行建模，因此可视化权重可能只显示一些模糊的区域，也可能只显示数字的某几个部分。
+
+此外，您可能还会看到一些基本上是噪点（这些噪点要么不收敛，要么被更高的层忽略）的神经元。
+
+在迭代不同的次数后停止训练并查看效果，可能会发现有趣的结果。
+
+"""
