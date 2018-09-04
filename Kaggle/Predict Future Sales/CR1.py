8b50fb2ea402728346a47a27c2ce5e1bded60f4c
traviszeng <traviszeng7@gmail.com>
2018-09-04 11:29:55 +0800

    add draft 1

---
+++ Kaggle/Predict Future Sales/CR1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows',500)
+pd.set_option('display.max_columns',100)
+
+from itertools import product
+from sklearn.preprocessing import LabelEncoder
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+#matplotlib inline
+
+
+from xgboost import XGBRegressor
+from xgboost import plot_importance
+
+def plot_features(booster,figsize):
+    fig,ax = plt.subplot(1,1,figsize=figsize)
+    return plot_importance(booster=booster,ax=ax)
+
+import time
+import sys
+import gc
+import pickle
+
+#print(sys.version_info)
+
+#加载数据
+items = pd.read_csv('items.csv')
+shops = pd.read_csv('shops.csv')
+cats = pd.read_csv('item_categories.csv')
+train = pd.read_csv('sales_train.csv.gz')
+#将ID设为index
+test = pd.read_csv('test.csv.gz').set_index('ID')
+
+#用seaborn的boxplot查看变量分布情况
+plt.figure(figsize=(10,4))
+plt.xlim(train.item_cnt_day.min(),train.item_cnt_day.max()*1.1)
+sns.boxplot(x=train.item_cnt_day)
+
+plt.figure(figsize=(10,4))
+plt.xlim(train.item_price.min(), train.item_price.max()*1.1)
+sns.boxplot(x=train.item_price)
+
+#决定移除item_cnt_day>1000和item_price>1000000的记录
+train = train[train.item_cnt_day<100001]
+train = train[train.item_price<1001]
