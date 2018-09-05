c2663f9f9724d975ac22a4020352c2bef8d28ade
traviszeng <traviszeng7@gmail.com>
2018-09-05 11:29:23 +0800

    finish mean feature

--- Kaggle/Predict Future Sales/CR1.py
+++ Kaggle/Predict Future Sales/CR1.py
@@ -59,3 +59,233 @@ test.loc[test.shop_id == 1, 'shop_id'] = 58
 # Жуковский ул. Чкалова 39м²
 train.loc[train.shop_id == 10, 'shop_id'] = 11
 test.loc[test.shop_id == 10, 'shop_id'] = 11
+
+#预处理shops/cats/items
+#shop名字第一项为城市名，将城市名和商店名split
+shops.loc[shops.shop_name == 'Сергиев Посад ТЦ "7Я"', 'shop_name'] = 'СергиевПосад ТЦ "7Я"'
+shops['city'] = shops['shop_name'].str.split(' ').map(lambda x: x[0])
+shops.loc[shops.city == '!Якутск', 'city'] = 'Якутск'
+#添加city_code
+shops['city_code'] = LabelEncoder().fit_transform(shops['city'])
+shops = shops[['shop_id','city_code']]
+
+#cat中包含item类别和子类别
+cats['split'] = cats['item_category_name'].str.split('-')
+cats['type'] = cats['split'].map(lambda x: x[0].strip())
+cats['type_code'] = LabelEncoder().fit_transform(cats['type'])
+# if subtype is nan then type
+cats['subtype'] = cats['split'].map(lambda x: x[1].strip() if len(x) > 1 else x[0].strip())
+cats['subtype_code'] = LabelEncoder().fit_transform(cats['subtype'])
+cats = cats[['item_category_id','type_code', 'subtype_code']]
+
+items.drop(['item_name'], axis=1, inplace=True)
+
+#1312 items are new compared to the train
+len(list(set(test.item_id) - set(test.item_id).intersection(set(train.item_id)))), len(list(set(test.item_id))), len(test)
+
+#将每个月商店卖出的东西存在matrix
+ts = time.time()
+matrix = []
+cols = ['date_block_num', 'shop_id', 'item_id']
+for i in range(34):
+    sales = train[train.date_block_num == i]
+    matrix.append(np.array(list(product([i], sales.shop_id.unique(), sales.item_id.unique())), dtype='int16'))
+
+matrix = pd.DataFrame(np.vstack(matrix), columns=cols)
+matrix['date_block_num'] = matrix['date_block_num'].astype(np.int8)
+matrix['shop_id'] = matrix['shop_id'].astype(np.int8)
+matrix['item_id'] = matrix['item_id'].astype(np.int16)
+matrix.sort_values(cols, inplace=True)
+print('using time '+str(time.time()-ts))
+
+ts = time.time()
+#按照date_block_num shop_id item_id的顺序为train里面的记录分组，计算出来每一个月每个商店中每样商品卖出的数量
+group = train.groupby(['date_block_num','shop_id','item_id']).agg({'item_cnt_day': ['sum']})
+#记为item_cnt_month
+group.columns = ['item_cnt_month']
+group.reset_index(inplace=True)
+
+#将group合并到matrix中
+matrix = pd.merge(matrix, group, on=cols, how='left')
+#将NaN变为0.0，锁定在0-20范围中
+matrix['item_cnt_month'] = (matrix['item_cnt_month']
+                                .fillna(0)
+                                .clip(0,20) # NB clip target here
+                                .astype(np.float16))
+print('using time '+str(time.time()-ts))
+
+#print(matrix)
+
+test['date_block_num'] = 34
+test['date_block_num'] = test['date_block_num'].astype(np.int8)
+test['shop_id'] = test['shop_id'].astype(np.int8)
+test['item_id'] = test['item_id'].astype(np.int16)
+
+ts = time.time()
+matrix = pd.concat([matrix, test], ignore_index=True, sort=False, keys=cols)
+#print(matrix.head(5))
+matrix.fillna(0, inplace=True) # 34 month
+#print(matrix.tail(5))
+print('using time '+str(time.time()-ts))
+
+#将cats items shops和matrix合并
+ts = time.time()
+matrix = pd.merge(matrix, shops, on=['shop_id'], how='left')
+matrix = pd.merge(matrix, items, on=['item_id'], how='left')
+matrix = pd.merge(matrix, cats, on=['item_category_id'], how='left')
+matrix['city_code'] = matrix['city_code'].astype(np.int8)
+matrix['item_category_id'] = matrix['item_category_id'].astype(np.int8)
+matrix['type_code'] = matrix['type_code'].astype(np.int8)
+matrix['subtype_code'] = matrix['subtype_code'].astype(np.int8)
+print('using time '+str(time.time()-ts))
+
+#添加过去1 2 3 6 12个月的销售情况列（过去的销售情况影响现在）
+def lag_feature(df, lags, col):
+    tmp = df[['date_block_num','shop_id','item_id',col]]
+    for i in lags:
+        shifted = tmp.copy()
+        shifted.columns = ['date_block_num','shop_id','item_id', col+'_lag_'+str(i)]
+        shifted['date_block_num'] += i
+        df = pd.merge(df, shifted, on=['date_block_num','shop_id','item_id'], how='left')
+    return df
+
+ts = time.time()
+matrix = lag_feature(matrix, [1,2,3,6,12], 'item_cnt_month')
+#print(matrix.head(5))
+print('using time '+str(time.time()-ts))
+
+"""
+计算平均值特征
+"""
+#每个月店铺出售商品数目平均值
+ts = time.time()
+group = matrix.groupby(['date_block_num']).agg({'item_cnt_month': ['mean']})
+group.columns = [ 'date_avg_item_cnt' ]
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num'], how='left')
+matrix['date_avg_item_cnt'] = matrix['date_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_avg_item_cnt')
+matrix.drop(['date_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+#每个月每样商品出售的均值
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'item_id']).agg({'item_cnt_month': ['mean']})
+group.columns = [ 'date_item_avg_item_cnt' ]
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num','item_id'], how='left')
+matrix['date_item_avg_item_cnt'] = matrix['date_item_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1,2,3,6,12], 'date_item_avg_item_cnt')
+matrix.drop(['date_item_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+#每个月每个店铺出售的均值
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'shop_id']).agg({'item_cnt_month': ['mean']})
+group.columns = [ 'date_shop_avg_item_cnt' ]
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num','shop_id'], how='left')
+matrix['date_shop_avg_item_cnt'] = matrix['date_shop_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1,2,3,6,12], 'date_shop_avg_item_cnt')
+matrix.drop(['date_shop_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+#每个月每类商品出售的均值
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'item_category_id']).agg({'item_cnt_month': ['mean']})
+group.columns = [ 'date_cat_avg_item_cnt' ]
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num','item_category_id'], how='left')
+matrix['date_cat_avg_item_cnt'] = matrix['date_cat_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_cat_avg_item_cnt')
+matrix.drop(['date_cat_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+#每个月每类商品在每个商店出售的均值
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'shop_id', 'item_category_id']).agg({'item_cnt_month': ['mean']})
+group.columns = ['date_shop_cat_avg_item_cnt']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num', 'shop_id', 'item_category_id'], how='left')
+matrix['date_shop_cat_avg_item_cnt'] = matrix['date_shop_cat_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_shop_cat_avg_item_cnt')
+matrix.drop(['date_shop_cat_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'shop_id', 'type_code']).agg({'item_cnt_month': ['mean']})
+group.columns = ['date_shop_type_avg_item_cnt']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num', 'shop_id', 'type_code'], how='left')
+matrix['date_shop_type_avg_item_cnt'] = matrix['date_shop_type_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_shop_type_avg_item_cnt')
+matrix.drop(['date_shop_type_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+#子类别 商店 每个月
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'shop_id', 'subtype_code']).agg({'item_cnt_month': ['mean']})
+group.columns = ['date_shop_subtype_avg_item_cnt']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num', 'shop_id', 'subtype_code'], how='left')
+matrix['date_shop_subtype_avg_item_cnt'] = matrix['date_shop_subtype_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_shop_subtype_avg_item_cnt')
+matrix.drop(['date_shop_subtype_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+#每个月每个城市均值
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'city_code']).agg({'item_cnt_month': ['mean']})
+group.columns = [ 'date_city_avg_item_cnt' ]
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num', 'city_code'], how='left')
+matrix['date_city_avg_item_cnt'] = matrix['date_city_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_city_avg_item_cnt')
+matrix.drop(['date_city_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+#每个月每座城市每种商品的均值
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'item_id', 'city_code']).agg({'item_cnt_month': ['mean']})
+group.columns = [ 'date_item_city_avg_item_cnt' ]
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num', 'item_id', 'city_code'], how='left')
+matrix['date_item_city_avg_item_cnt'] = matrix['date_item_city_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_item_city_avg_item_cnt')
+matrix.drop(['date_item_city_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+
+#每个月每类商品均值
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'type_code']).agg({'item_cnt_month': ['mean']})
+group.columns = [ 'date_type_avg_item_cnt' ]
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num', 'type_code'], how='left')
+matrix['date_type_avg_item_cnt'] = matrix['date_type_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_type_avg_item_cnt')
+matrix.drop(['date_type_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
+
+#每个月每种子类型商品均值
+ts = time.time()
+group = matrix.groupby(['date_block_num', 'subtype_code']).agg({'item_cnt_month': ['mean']})
+group.columns = [ 'date_subtype_avg_item_cnt' ]
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num', 'subtype_code'], how='left')
+matrix['date_subtype_avg_item_cnt'] = matrix['date_subtype_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_subtype_avg_item_cnt')
+matrix.drop(['date_subtype_avg_item_cnt'], axis=1, inplace=True)
+print('using time '+str(time.time()-ts))
