1fe47b1ae4227a1fa337804db2b5b3dbefd18f1a
traviszeng <traviszeng7@gmail.com>
2018-09-16 17:55:44 +0800

    finish validation and model optimization

--- PythonML/validation_and_model.py
+++ PythonML/validation_and_model.py
@@ -211,9 +211,180 @@ def main():
     print('Test accuracy: %3f' % clf.score(X_test,y_test))
     #也可以使用RandomizedSearchCV 抽取出随机参数组合
 
+    #通过嵌套交叉验证选择不同机器学习算法
+    print('7.Test nested cross validation'+20*'-')
+
+    gs = GridSearchCV(estimator=pipe_svc,
+                      param_grid=param_grid,
+                      scoring='accuracy',
+                      cv=2)
+
+    scores = cross_val_score(gs,X_train,y_train,scoring='accuracy',cv=5)
+
+    print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores),
+                                      np.std(scores)))
+
+    #使用嵌套交叉验证方法比较SVM和简单决策树模型
+    from sklearn.tree import DecisionTreeClassifier
+    #只调优树的深度
+    gs = GridSearchCV(estimator=DecisionTreeClassifier(random_state=0),
+                      param_grid=[{'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}],
+                      scoring='accuracy',
+                      cv=2)
+
+    scores = cross_val_score(gs, X_train, y_train,
+                             scoring='accuracy', cv=5)
+    print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores),
+                                          np.std(scores)))
 
 
+    #混淆矩阵
+    print('8.Confusion matrix'+20*'-')
+    from sklearn.metrics import confusion_matrix
 
+    pipe_svc.fit(X_train, y_train)
+    y_pred = pipe_svc.predict(X_test)
+    confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    print(confmat)
+
+    #用matplotlib中的matshow标识出来
+    fig, ax = plt.subplots(figsize=(2.5, 2.5))
+    ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+    for i in range(confmat.shape[0]):
+        for j in range(confmat.shape[1]):
+            ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+
+    plt.xlabel('Predicted label')
+    plt.ylabel('True label')
+    plt.tight_layout()
+    plt.show()
+
+    le.transform(['M', 'B'])
+    confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    print(confmat)
+
+    from sklearn.metrics import precision_score, recall_score, f1_score
+
+    print('Precision: %.3f' % precision_score(y_true=y_test, y_pred=y_pred))
+    print('Recall: %.3f' % recall_score(y_true=y_test, y_pred=y_pred))
+    print('F1: %.3f' % f1_score(y_true=y_test, y_pred=y_pred))
+
+    #用自己定义的评分来找到最优超参组合
+    from sklearn.metrics import make_scorer
+
+    scorer = make_scorer(f1_score, pos_label=0)
+
+    c_gamma_range = [0.01, 0.1, 1.0, 10.0]
+
+    param_grid = [{'svc__C': c_gamma_range,
+                   'svc__kernel': ['linear']},
+                  {'svc__C': c_gamma_range,
+                   'svc__gamma': c_gamma_range,
+                   'svc__kernel': ['rbf']}]
+
+    gs = GridSearchCV(estimator=pipe_svc,
+                      param_grid=param_grid,
+                      scoring=scorer,
+                      cv=10,
+                      n_jobs=-1)
+    gs = gs.fit(X_train, y_train)
+    print(gs.best_score_)
+    print(gs.best_params_)
+
+    print('9.Print a ROC'+20*'-')
+    from sklearn.metrics import roc_curve, auc
+    from scipy import interp
+
+    pipe_lr = make_pipeline(StandardScaler(),
+                            PCA(n_components=2),
+                            LogisticRegression(penalty='l2',
+                                               random_state=1,
+                                               C=100.0))
+
+    X_train2 = X_train[:, [4, 14]]
+
+    cv = list(StratifiedKFold(n_splits=3,
+                              random_state=1).split(X_train, y_train))
+
+    fig = plt.figure(figsize=(7, 5))
+
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    all_tpr = []
+
+    for i, (train, test) in enumerate(cv):
+        probas = pipe_lr.fit(X_train2[train],
+                             y_train[train]).predict_proba(X_train2[test])
+
+        fpr, tpr, thresholds = roc_curve(y_train[test],
+                                         probas[:, 1],
+                                         pos_label=1)
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr,
+                 tpr,
+                 label='ROC fold %d (area = %0.2f)'
+                       % (i + 1, roc_auc))
+
+    plt.plot([0, 1],
+             [0, 1],
+             linestyle='--',
+             color=(0.6, 0.6, 0.6),
+             label='random guessing')
+
+    mean_tpr /= len(cv)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    plt.plot(mean_fpr, mean_tpr, 'k--',
+             label='mean ROC (area = %0.2f)' % mean_auc, lw=2)
+    plt.plot([0, 0, 1],
+             [0, 1, 1],
+             linestyle=':',
+             color='black',
+             label='perfect performance')
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('false positive rate')
+    plt.ylabel('true positive rate')
+    plt.legend(loc="lower right")
+
+    plt.tight_layout()
+    # plt.savefig('images/06_10.png', dpi=300)
+    plt.show()
+
+    #多类别分类的评价标准
+    print('10. The scoring metrics for multiclass classification'+20*'-')
+    pre_scorer = make_scorer(score_func=precision_score,
+                             pos_label=1,
+                             greater_is_better=True,
+                             average='micro')
+
+    #Dealing with class imbalances
+    X_imb = np.vstack((X[y == 0], X[y == 1][:40]))
+    y_imb = np.hstack((y[y == 0], y[y == 1][:40]))
+
+    y_pred = np.zeros(y_imb.shape[0])
+    np.mean(y_pred == y_imb) * 100
+
+    from sklearn.utils import resample
+
+    print('Number of class 1 samples before:', X_imb[y_imb == 1].shape[0])
+
+    X_upsampled, y_upsampled = resample(X_imb[y_imb == 1],
+                                        y_imb[y_imb == 1],
+                                        replace=True,
+                                        n_samples=X_imb[y_imb == 0].shape[0],
+                                        random_state=123)
+
+    print('Number of class 1 samples after:', X_upsampled.shape[0])
+
+    X_bal = np.vstack((X[y == 0], X_upsampled))
+    y_bal = np.hstack((y[y == 0], y_upsampled))
+
+    y_pred = np.zeros(y_bal.shape[0])
+    np.mean(y_pred == y_bal) * 100
 
 
 
