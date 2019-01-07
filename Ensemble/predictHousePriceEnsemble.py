b1945cc7093a8b4845729d0e3da486a4737965b1
traviszeng <traviszeng7@gmail.com>
2019-01-07 21:56:37 +0800

    add ensemble

--- Ensemble/predictHousePriceEnsemble.py
+++ Ensemble/predictHousePriceEnsemble.py
@@ -242,14 +242,17 @@ Modeling
 
 from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
 from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
+from sklearn.svm import SVR
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import RobustScaler
 from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
 import xgboost as xgb
 import lightgbm as lgb
+import copy
 
 #Validation function
 n_folds = 5
@@ -262,6 +265,11 @@ def rmsle_cv(model):
 """
 Base models
 """
+#support vector regression
+svr = make_pipeline(RobustScaler(),SVR(C=1.0,epsilon=0.2))
+
+#Random forest regression
+rfr = make_pipeline(RobustScaler(), RandomForestRegressor(n_estimators=20,random_state=0))
 """
 LASSO Regression :
 This model may be very sensitive to outliers. 
@@ -303,6 +311,12 @@ model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
 #SCORES
 #Let's see how these base models perform
 # on the data by evaluating the cross-validation rmsle error
+score = rmsle_cv(svr)
+print("\nSupport vector regression score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
+score = rmsle_cv(rfr)
+print("\nRandom forest score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
 score = rmsle_cv(lasso)
 print("\nLasso score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
 
@@ -322,3 +336,191 @@ print("Xgboost score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
 score = rmsle_cv(model_lgb)
 print("LGBM score: {:.4f} ({:.4f})\n" .format(score.mean(), score.std()))
 
+"""
+Bagging
+
+using averaging
+"""
+
+
+class baggingAveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models):
+        self.models = models
+
+    # we define clones of the original models to fit the data in
+    def fit(self, X, y):
+        self.models_ = [clone(x) for x in self.models]
+
+        # Train cloned base models
+        for model in self.models_:
+            model.fit(X, y)
+
+        return self
+
+    # Now we do the predictions for cloned models and average them
+    def predict(self, X):
+        predictions = np.column_stack([
+            model.predict(X) for model in self.models_
+        ])
+        return np.mean(predictions, axis=1)
+
+#使用ENet, GBoost, KRR and lasso做一个bagging
+averaged_models = baggingAveragingModels(models = (ENet, GBoost, KRR, lasso))
+
+score = rmsle_cv(averaged_models)
+print(" Averaged base models score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
+#全部做bagging
+averaged_models = baggingAveragingModels(models = (ENet, GBoost, KRR, lasso,rfr,svr))
+
+score = rmsle_cv(averaged_models)
+print(" Averaged base models score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
+
+"""
+Stacking
+"""
+
+
+class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.n_folds = n_folds
+
+    # We again fit the data on clones of the original models
+    def fit(self, X, y):
+        self.base_models_ = [list() for x in self.base_models]
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
+
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for train_index, holdout_index in kfold.split(X, y):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+                instance.fit(X[train_index], y[train_index])
+                y_pred = instance.predict(X[holdout_index])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+
+        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+
+    # Do the predictions of all base models on the test data and use the averaged predictions as
+    # meta-features for the final prediction which is done by the meta-model
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_])
+        return self.meta_model_.predict(meta_features)
+
+
+stacked_averaged_models = StackingAveragedModels(base_models = (ENet, GBoost, KRR),
+                                                 meta_model = lasso)
+best_stacked_model = copy.deepcopy(stacked_averaged_models)
+
+score = rmsle_cv(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+stacked_averaged_models = StackingAveragedModels(base_models = (lasso, GBoost, KRR),
+                                                 meta_model = ENet)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+stacked_averaged_models = StackingAveragedModels(base_models = (lasso, GBoost, ENet),
+                                                 meta_model = KRR)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+stacked_averaged_models = StackingAveragedModels(base_models = (lasso, ENet, KRR),
+                                                 meta_model = GBoost)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+print("-----------Including all single learner-----------------")
+stacked_averaged_models = StackingAveragedModels(base_models = (lasso, ENet, KRR,svr,rfr),
+                                                 meta_model = GBoost)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+stacked_averaged_models = StackingAveragedModels(base_models = (lasso, ENet, KRR,svr,GBoost),
+                                                 meta_model = rfr)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+stacked_averaged_models = StackingAveragedModels(base_models = (lasso, ENet, GBoost,svr,rfr),
+                                                 meta_model = KRR)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+stacked_averaged_models = StackingAveragedModels(base_models = (lasso, ENet, GBoost,KRR,rfr),
+                                                 meta_model = svr)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+
+stacked_averaged_models = StackingAveragedModels(base_models = (lasso, KRR, GBoost,svr,rfr),
+                                                 meta_model = ENet)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+stacked_averaged_models3 = StackingAveragedModels(base_models = (KRR, ENet, GBoost,svr,rfr),
+                                                 meta_model = lasso)
+
+score = rmsle_cv(stacked_averaged_models)
+if rmsle_cv(best_stacked_model).mean()>score.mean():
+    best_stacked_model = copy.deepcopy(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
+
+#We first define a rmsle evaluation function
+def rmsle(y, y_pred):
+    return np.sqrt(mean_squared_error(y, y_pred))
+
+#找到一个分数最佳的
+best_stacked_model.fit(train.values, y_train)
+stacked_train_pred = best_stacked_model.predict(train.values)
+stacked_pred = np.expm1(best_stacked_model.predict(test.values))
+print(rmsle(y_train, stacked_train_pred))
+
+model_xgb.fit(train, y_train)
+xgb_train_pred = model_xgb.predict(train)
+xgb_pred = np.expm1(model_xgb.predict(test))
+print(rmsle(y_train, xgb_train_pred))
+
+model_lgb.fit(train, y_train)
+lgb_train_pred = model_lgb.predict(train)
+lgb_pred = np.expm1(model_lgb.predict(test.values))
+print(rmsle(y_train, lgb_train_pred))
+
+'''RMSE on the entire Train data when averaging'''
+
+print('RMSLE score on train data:')
+print(rmsle(y_train,stacked_train_pred*0.70 +
+               xgb_train_pred*0.15 + lgb_train_pred*0.15 ))
