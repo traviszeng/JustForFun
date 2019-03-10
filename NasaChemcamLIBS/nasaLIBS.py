999f0478d2843bd98232f42db88298f56e531e06
traviszeng <traviszeng7@gmail.com>
2019-03-10 22:05:42 +0800

    deal with columns issue

--- NasaChemcamLIBS/nasaLIBS.py
+++ NasaChemcamLIBS/nasaLIBS.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 DATA_base_add = "E:\\NasaChemcamLIBSDataset\\"
 DATA_folder_add = "E:\\NasaChemcamLIBSDataset\\Derived\\ica\\"
@@ -10,6 +12,15 @@ def loadConcentrationData():
     con = pd.read_csv(DATA_base_add+"Concentration.csv")
     #print(con)
     return con
+
+#处理初始数据文件的一些问题
+def processRawData(data):
+    data = data.loc[1:6144]
+    #处理一下columns的问题
+    for n in data.columns:
+        n.replace('#',' ')
+        n.strip()
+    return data
 
 
 def loadTrainingSamples():
@@ -21,7 +32,8 @@ def loadTrainingSamples():
                     #print(os.path.join(root, name))
                     if file.endswith(".csv"):
                         print("Reading files in "+os.path.join(root, file))
-                        data = pd.read_csv(os.path.join(root, file),skiprows=14)
+                        data = pd.read_csv(os.path.join(root, file),skiprows=16)
+                        data = processRawData(data)
                         if name not in trainingData.keys():
                             trainingData[name] = []
                             trainingData[name].append(data)
